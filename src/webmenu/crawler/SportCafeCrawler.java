051b698c73d19cd5121b9c8839013e7c14f10c62
Miroslav Bajtos <bajtos.miroslav.jr@gmail.com>
2010-01-16 18:45:00 +0100

    Enabled dev-checks in SportCafeCrawler.

--- src/webmenu/crawler/SportCafeCrawler.java
+++ src/webmenu/crawler/SportCafeCrawler.java
@@ -8,42 +8,38 @@ import java.util.logging.Logger;
 import webmenu.model.*;
 import webmenu.data.*;
 
-public class SportCafeCrawler implements Crawler
-{
-    private static final Logger log = Logger.getLogger(SportCafeCrawler.class.getName());
+public class SportCafeCrawler implements Crawler {
+   private static final Logger log = Logger.getLogger(SportCafeCrawler.class.getName());
 
-    protected UrlFetcher createFetcher()
-    {
-        return new GoogleUrlFetcher();
-    }
+   protected UrlFetcher createFetcher() {
+      return new GoogleUrlFetcher();
+   }
 
-    protected Parser createParser()
-    {
-        return new SportCafeParser();
-    }
+   protected Parser createParser() {
+      return new SportCafeParser();
+   }
 
-    protected OneDayMenuStore createStore()
-    {
-        return new GoogleOneDayMenuStore();
-    }
+   protected OneDayMenuStore createStore() {
+      return new GoogleOneDayMenuStore();
+   }
 
-    public void update(URL url) throws MalformedURLException, IOException, CrawlException
-    {
-        if (url == null) url = new URL("http://www.sport-cafe.cz/menu.php");
-        log.fine("Using url '" + url + "'");
+   public void update(URL url) throws MalformedURLException, IOException, CrawlException {
+      boolean defaultUrl = url == null;
+      if (defaultUrl) url = new URL("http://www.sport-cafe.cz/menu.php");
+      log.fine("Using url '" + url + "'");
 
-        InputStream webpage = createFetcher().fetch(url);
-        OneDayMenu[] data = createParser().parse(webpage);
-        OneDayMenuStore store = createStore();
+      InputStream webpage = createFetcher().fetch(url);
+      OneDayMenu[] data = createParser().parse(webpage);
+      OneDayMenuStore store = createStore();
 
-        Calendar now = Calendar.getInstance();
-        Calendar today = new GregorianCalendar(now.get(Calendar.YEAR), now.get(Calendar.MONTH), now.get(Calendar.DAY_OF_MONTH));
+      Calendar now = Calendar.getInstance();
+      Calendar today = new GregorianCalendar(now.get(Calendar.YEAR), now.get(Calendar.MONTH), now.get(Calendar.DAY_OF_MONTH));
 
-        for (OneDayMenu menu : data)
-        {
-            if (menu.getDay().before(today.getTime()))
-                continue;
-            store.updateOneDayMenu(Restaurants.SPORT_CAFE_HK, menu);
-        }
-    }
+      for (OneDayMenu menu : data) {
+         // non-default url are used for development checks
+         if (menu.getDay().before(today.getTime()) && defaultUrl)
+            continue;
+         store.updateOneDayMenu(Restaurants.SPORT_CAFE_HK, menu);
+      }
+   }
 }
